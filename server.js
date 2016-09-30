491d5e202fd17be7c025e68a36012c038b14ab29
Vinod T R <vinodtr@gmail.com>
2016-09-30 08:00:50 -0500

    Added infrastructure for socket.io

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
 var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+io.on('connection', function() {
+	console.log("User connected via socket.io");
+});
 
 app.use(express.static(__dirname + '/public'));
 http.listen(PORT, function() {
 	console.log("Server started");
-})
+});
