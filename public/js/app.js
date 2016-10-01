7d66b859a78e915f341318677c0eb422940ae9c7
Vinod T R <vinodtr@gmail.com>
2016-10-01 07:19:56 -0500

    Added files for the chat application

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,7 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to Socket I/O server');
 });
+
+socket.on('message', function(message) {
+	console.log('New message arrived : ' + message.text);
+});
--- server.js
+++ server.js
@@ -4,9 +4,20 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
-io.on('connection', function() {
+io.on('connection', function(socket) {
 	console.log("User connected via socket.io");
+
+	socket.on('message', function(message) {
+		console.log('New message arrived from client: ' + message.text);
+		socket.broadcast.emit('message', message);
+	});
+
+	io.emit('message', {
+		text: "Welcome to the chat application!"
+	})
 });
+
+
 
 app.use(express.static(__dirname + '/public'));
 http.listen(PORT, function() {
